df8f51c065cf8d4d2f84ded56e4f86ba31bc9dcf
Serhii Kushneruk <svkushneruk@ukr.net>
2022-05-31 19:20:56 +0300

    Додає д/з 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  startBtnEl: document.querySelector('button[data-start]'),
+  stopBtnEl: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtnEl.addEventListener('click', onStartBtnClick);
+refs.stopBtnEl.addEventListener('click', onStopBtnClick);
+
+let startId = null;
+
+function onStartBtnClick() {
+  this.disabled = true;
+  startId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  refs.startBtnEl.disabled = false;
+  clearInterval(startId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
